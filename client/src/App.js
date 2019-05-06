8e47b53ea6d32fec1e21ac0c04527b3e33231a0e
ashishthesatan <ashishthesatan@gmail.com>
2019-05-06 17:39:54 +0545

    integrated bootstrap styles in react project

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,25 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <form action="post">
-        <input type="text" />
-      </form>
+    <div className="container">
+      <div className="card">
+        <div className="card-header">Input Form</div>
+        <div className="card-body">
+          <form>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="form-control mb-2"
+            />
+            <input
+              type="password"
+              className="form-control mb-2"
+              placeholder="Enter password"
+            />
+            <button className="btn btn-primary form-control">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
